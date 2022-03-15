317b47c13c271ad246a0a8684fdb4a24b81ae6c6
asasincreedmc <asasincreedmc@gmail.com>
2022-03-15 00:40:08 -0300

    Properly change grid size when resetting it

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ document.head.appendChild(styleGrid)
 const styles = styleGrid.sheet
 const resetGridBtn = document.querySelector(".reset")
 let squaresPerSide = 16;
-generateGrid(16)
-styles.insertRule(".container { grid-template-columns: repeat(16, 1fr); }");
+generateDefaultGrid();
 
 resetGridBtn.addEventListener("click", () => {
     let input = prompt("Select the number of squares per side: ", 16)
     if (input <= 100 && input > 0) {
         eraseGrid();
         generateGrid(input);
+        reziseGrid(input);
     } else if (input > 100) {
         alert("That number is too big (Max 100)");
     } else alert("Please enter a valid number");
@@ -36,4 +36,15 @@ function eraseGrid() {
     for (div of divs) {
         container.removeChild(div)
     }
+}
+
+function reziseGrid(sideSize) {
+    styles.deleteRule(0)
+    styles.insertRule(`.container { grid-template-columns: repeat(${sideSize}, 1fr); }`, 0);
+    squaresPerSide = sideSize
+}
+
+function generateDefaultGrid() {
+    generateGrid(16)
+    styles.insertRule(".container { grid-template-columns: repeat(16, 1fr); }", 0);
 }
