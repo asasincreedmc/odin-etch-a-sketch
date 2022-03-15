841fa49db1a1227d9261c991a4cf30043fc0910b
asasincreedmc <asasincreedmc@gmail.com>
2022-03-14 23:46:12 -0300

    Erase grid and generate a new one without changing grid size

--- script.js
+++ script.js
@@ -2,8 +2,22 @@ const container = document.querySelector(".container");
 const styleGrid = document.createElement("style")
 document.head.appendChild(styleGrid)
 const styles = styleGrid.sheet
-
+const resetGridBtn = document.querySelector(".reset")
+let squaresPerSide = 16;
+generateGrid(16)
 styles.insertRule(".container { grid-template-columns: repeat(16, 1fr); }");
+
+resetGridBtn.addEventListener("click", () => {
+    let input = prompt("Select the number of squares per side: ", 16)
+    if (input <= 100 && input > 0) {
+        eraseGrid();
+        generateGrid(input);
+    } else if (input > 100) {
+        alert("That number is too big (Max 100)");
+    } else alert("Please enter a valid number");
+
+}
+)
 
 function generateSquare() {
     const div = document.createElement("div")
@@ -11,6 +25,15 @@ function generateSquare() {
     container.appendChild(div)
 }
 
-for (let i = 0; i < (16 * 16); i++) {
-    generateSquare()
+function generateGrid(sideSize) {
+    for (let i = 0; i < (sideSize * sideSize); i++) {
+        generateSquare()
+    }
+}
+
+function eraseGrid() {
+    let divs = document.querySelectorAll(".grid")
+    for (div of divs) {
+        container.removeChild(div)
+    }
 }
